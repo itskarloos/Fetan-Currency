8a667e3a12314844347c53b51b0d14c1ee9c98b0
mikiseyoum7 <mikiseyoum7@gmail.com>
2024-09-01 15:26:59 +0000

    Toggle Fix

--- components/shared/ThemeToggle.tsx
+++ components/shared/ThemeToggle.tsx
@@ -5,8 +5,8 @@ import { IconSun } from '@tabler/icons-react';
 
 const ThemeToggleButton = () => (
    <button onClick={toggleDarkMode}>
-          <Toggle variant="outline" className="dark:background-white dark:text-black text-grey-500"aria-label="Toggle theme">
-            <IconSun className="h-4 w-4" />
+          <Toggle variant="outline" className="dark:background-white dark:text-black "aria-label="Toggle theme">
+            <IconSun className="h-4 w-4 text-grey-500" />
           </Toggle>
         </button>
 );
