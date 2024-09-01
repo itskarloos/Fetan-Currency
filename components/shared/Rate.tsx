2637940470482f7729b390950825b94fed192e99
mikiseyoum7 <mikiseyoum7@gmail.com>
2024-09-01 16:13:59 +0000

    Exchange Box Initial Commit

--- components/shared/Rate.tsx
+++ components/shared/Rate.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ExchangeCard from './ExchangeCard'
 
 const Rate = () => {
   return (
-    <div>Rate</div>
+    <div className="w-full flex flex-col">
+      <p className="text-base md:text-lg lg:text-xl xl:text-2xl text-white">
+        Exchange Rate
+      </p>
+      <ExchangeCard />
+    </div>
   )
 }
 
