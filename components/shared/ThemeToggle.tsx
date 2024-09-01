492f101da77eacdfd5417d92b4c458b124fbbdd5
mikiseyoum7 <mikiseyoum7@gmail.com>
2024-09-01 15:20:29 +0000

    dark mode toggle

--- components/shared/ThemeToggle.tsx
+++ components/shared/ThemeToggle.tsx
@@ -1,10 +1,14 @@
 import { toggleDarkMode } from '@/utils/theme-toggle';
 import { Button } from '../ui/button';
+import { Toggle } from '../ui/toggle';
+import { IconSun } from '@tabler/icons-react';
 
 const ThemeToggleButton = () => (
-  <Button className="bg-white" onClick={toggleDarkMode}>
-    Toggle Dark Mode
-  </Button>
+   <button onClick={toggleDarkMode}>
+          <Toggle variant="outline" className="dark:background-white dark:text-black text-grey-500"aria-label="Toggle theme">
+            <IconSun className="h-4 w-4" />
+          </Toggle>
+        </button>
 );
 export default ThemeToggleButton
  
