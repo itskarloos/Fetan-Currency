6fa0159f2e9f1a128f8285b0e22479b0f8bbf8ed
mikiseyoum7 <mikiseyoum7@gmail.com>
2024-09-01 16:29:31 +0000

    ComboBox

--- components/shared/Rate.tsx
+++ components/shared/Rate.tsx
@@ -3,7 +3,7 @@ import ExchangeCard from './ExchangeCard'
 
 const Rate = () => {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex flex-col mt-10 gap-2">
       <p className="text-base md:text-lg lg:text-xl xl:text-2xl text-white">
         Exchange Rate
       </p>
