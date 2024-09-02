5da4eca617aeb92936e3e3d352c1ed442ff8692e
mikiseyoum7 <mikiseyoum7@gmail.com>
2024-09-02 18:12:13 +0000

    Structure Change

--- components/shared/ExchangeCard.tsx
+++ components/shared/ExchangeCard.tsx
@@ -1,9 +1,9 @@
 
 "use client"
- 
+
 import * as React from "react"
 import { Check, ChevronsUpDown } from "lucide-react"
- 
+
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
@@ -54,102 +54,102 @@ const ExchangeCard = () => {
     <>
 
 
-    <Popover open={open} onOpenChange={setOpen}>
-    <PopoverTrigger asChild>
-      <Button
-        variant="outline"
-        role="combobox"
-        aria-expanded={open}
-        className="w-[full] md:w-[200px] justify-between text-gray-300 bg-black border-gray-700 hover:bg-gray-700 hover:text-gray-200"
-      >
-        {value
-          ? banks.find((bank) => bank.value === value)?.label
-          : "Select Bank..."}
-        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-      </Button>
-    </PopoverTrigger>
-    <PopoverContent className="w-[200px] p-0 bg-black border-gray-700">
-      <Command className="bg-black">
-        <CommandInput placeholder="Search Banks..." className=" text-gray-300" />
-        <CommandList>
-          <CommandEmpty className="text-gray-400">No Banks Found</CommandEmpty>
-          <CommandGroup>
-            {banks.map((bank) => (
-              <CommandItem
-                key={bank.value}
-                value={bank.value}
-                onSelect={(currentValue) => {
-                  setValue(currentValue === value ? "" : currentValue)
-                  setOpen(false)
-                }}
-                className="text-white"
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    value === bank.value ? "opacity-100" : "opacity-0"
-                  )}
-                />
-                {bank.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </CommandList>
-      </Command>
-    </PopoverContent>
-  </Popover>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[full] md:w-[200px] justify-between text-gray-300 bg-black border-gray-700 hover:bg-gray-700 hover:text-gray-200"
+          >
+            {value
+              ? banks.find((bank) => bank.value === value)?.label
+              : "Select Bank..."}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0 bg-black border-gray-700">
+          <Command className="bg-black">
+            <CommandInput placeholder="Search Banks..." className=" text-gray-300" />
+            <CommandList>
+              <CommandEmpty className="text-gray-400">No Banks Found</CommandEmpty>
+              <CommandGroup>
+                {banks.map((bank) => (
+                  <CommandItem
+                    key={bank.value}
+                    value={bank.value}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue)
+                      setOpen(false)
+                    }}
+                    className="text-white"
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === bank.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {bank.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
 
 
-  <Popover open={open} onOpenChange={setOpen}>
-    <PopoverTrigger asChild>
-      <Button
-        variant="outline"
-        role="combobox"
-        aria-expanded={open}
-        className="w-[full] md:w-[200px] justify-between text-gray-300 bg-black border-gray-700 hover:bg-gray-700 hover:text-gray-200"
-      >
-        {value
-          ? banks.find((bank) => bank.value === value)?.label
-          : "Select Bank..."}
-        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-      </Button>
-    </PopoverTrigger>
-    <PopoverContent className="w-[200px] p-0 bg-black border-gray-700">
-      <Command className="bg-black">
-        <CommandInput placeholder="Search Banks..." className=" text-gray-300" />
-        <CommandList>
-          <CommandEmpty className="text-gray-400">No Banks Found</CommandEmpty>
-          <CommandGroup>
-            {banks.map((bank) => (
-              <CommandItem
-                key={bank.value}
-                value={bank.value}
-                onSelect={(currentValue) => {
-                  setValue(currentValue === value ? "" : currentValue)
-                  setOpen(false)
-                }}
-                className="text-white"
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    value === bank.value ? "opacity-100" : "opacity-0"
-                  )}
-                />
-                {bank.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </CommandList>
-      </Command>
-    </PopoverContent>
-  </Popover>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[full] md:w-[200px] justify-between text-gray-300 bg-black border-gray-700 hover:bg-gray-700 hover:text-gray-200"
+          >
+            {value
+              ? banks.find((bank) => bank.value === value)?.label
+              : "Select Bank..."}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0 bg-black border-gray-700">
+          <Command className="bg-black">
+            <CommandInput placeholder="Search Banks..." className=" text-gray-300" />
+            <CommandList>
+              <CommandEmpty className="text-gray-400">No Banks Found</CommandEmpty>
+              <CommandGroup>
+                {banks.map((bank) => (
+                  <CommandItem
+                    key={bank.value}
+                    value={bank.value}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue)
+                      setOpen(false)
+                    }}
+                    className="text-white"
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === bank.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {bank.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
 
 
-  </>
+    </>
   )
 
-  
+
 }
 
 export default ExchangeCard
