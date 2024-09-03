90b73871b1812cc782febba048b459d2e35ae4f8
Michael <mikenegh@gmail.com>
2024-09-03 16:55:43 +0300

    Favicon

--- components/shared/Rate.tsx
+++ components/shared/Rate.tsx
@@ -5,7 +5,7 @@ import { Chart } from "./chart";
 const Rate = () => {
   return (
     <div className="h-[100] w-full flex bg-white dark:bg-black p-5 md:p-10 flex-col  gap-2 md:gap-6">
-      <p className="text-2xl md:text-3xl dark:text-neutral-400 text-neutral-600">
+      <p className="text-2xl md:text-3xl dark:text-neutral-400 text-neutral-900">
         Exchange Rate
       </p>
       <ExchangeCard />
