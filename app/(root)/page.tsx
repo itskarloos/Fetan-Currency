2b9d9a739663ccf678a76599a893ab5bda6009e2
Michael <mikenegh@gmail.com>
2024-09-06 15:13:30 +0300

    bug fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,9 @@ import React from "react";
 import NavBar from "@/components/shared/NavBar";
 import Hero from "@/components/shared/Hero";
 import Rate from "@/components/shared/Rate";
-import {
-  getLatestBankRates,
-  getNBEExchangeRates,
-} from "@/lib/database/actions/exchangeRate.actions";
+import { getLatestBankRates } from "@/lib/database/actions/exchangeRate.actions";
 
 export default async function Home() {
-  const nbeExchange = await getNBEExchangeRates();
   const latestBankRate = await getLatestBankRates();
 
   return (
