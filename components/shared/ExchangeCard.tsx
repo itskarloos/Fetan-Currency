c803cbc2a89c9b001b57e29347d274fff797771d
Michael <mikenegh@gmail.com>
2024-09-06 16:07:27 +0300

    space fix

--- components/shared/ExchangeCard.tsx
+++ components/shared/ExchangeCard.tsx
@@ -24,7 +24,7 @@ const ExchangeCard: React.FC = () => {
   const [currency, setCurrency] = React.useState("");
 
   return (
-    <div className="flex md:flex-row flex-col md:gap-6 space-y-3">
+    <div className="flex md:flex-row flex-col md:gap-6 space-y-5">
       <Select
         options={banks}
         value={bank}
