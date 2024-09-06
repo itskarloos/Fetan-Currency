0fc94410992ec8217b8dfddcdd42464e99debc00
Michael <mikenegh@gmail.com>
2024-09-06 16:30:40 +0300

    cache error fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import NavBar from "@/components/shared/NavBar";
 import Hero from "@/components/shared/Hero";
 import Rate from "@/components/shared/Rate";
 import { getLatestBankRates } from "@/lib/database/actions/exchangeRate.actions";
+
+export const revalidate = 0; // This ensures the page is not cached
 
 export default async function Home() {
   const latestBankRate = await getLatestBankRates();
