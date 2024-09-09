b4053aa6c18826285bc920c3e8a2b2ef35cc7e08
Michael <mikenegh@gmail.com>
2024-09-09 11:11:43 +0300

    tofixed()

--- components/shared/ExchangeChart.tsx
+++ components/shared/ExchangeChart.tsx
@@ -84,18 +84,18 @@ const ExchangeChart = ({
   const Data = [
     {
       month: "Buying",
-      "National Bank": chartData?.nbe.cashBuying.toFixed(),
-      [formattedBank]: chartData?.comparedBank.cashBuying.toFixed(),
+      "National Bank": chartData?.nbe.cashBuying,
+      [formattedBank]: chartData?.comparedBank.cashBuying,
     },
     {
       month: "Selling",
-      "National Bank": chartData?.nbe.cashSelling.toFixed(),
-      [formattedBank]: chartData?.comparedBank.cashSelling.toFixed(),
+      "National Bank": chartData?.nbe.cashSelling,
+      [formattedBank]: chartData?.comparedBank.cashSelling,
     },
     {
       month: "Difference",
-      "National Bank": chartData?.difference.cashBuying.toFixed(),
-      [formattedBank]: chartData?.difference.cashSelling.toFixed(),
+      "National Bank": chartData?.difference.cashBuying,
+      [formattedBank]: chartData?.difference.cashSelling,
     },
   ];
 
@@ -195,7 +195,8 @@ const ExchangeChart = ({
       <CardFooter className="flex-col items-start gap-2 text-sm">
         <div className="flex gap-2 font-medium leading-1">
           {formattedBank} - Cash Buying Difference{" "}
-          {(chartData.difference.cashBuying).toFixed()} <TrendingUp className="h-4 w-4" />
+          {chartData.difference.cashBuying.toFixed()}{" "}
+          <TrendingUp className="h-4 w-4" />
         </div>
         <div className="leading-none text-muted-foreground">
           Analysis on {chartData.comparedBank.timestamp}
