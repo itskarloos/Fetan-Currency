c3c023f446ca5ac536904d7208f8ab64ea4e4594
Michael <mikenegh@gmail.com>
2024-09-26 19:44:01 +0300

    Loading page fix

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,11 @@
-export default function Loading() {
+export default function LoadingPage() {
   return (
-    <div className="flex items-center justify-center w-full h-screen dark:bg-black bg-white">
+    <div className="flex items-center justify-center w-full h-screen bg-black">
       <div className="w-16 h-16 relative">
         {[0, 1, 2].map((index) => (
           <div
             key={index}
-            className="w-full h-full rounded-full border-2 dark:border-neutral-800 border-neutral-200absolute"
+            className="w-full h-full rounded-full border-2 border-neutral-800 absolute inset-0"
             style={{
               borderLeftColor: "white",
               animation: `spin 1.5s cubic-bezier(0.55, 0.25, 0.25, 0.70) infinite`,
